7072d2ddecf6692b2c0a5b28732986f52b3333d2
Zeynep Kaya <zeyynepkyaa@gmail.com>
2025-12-06 23:06:27 +0300

    TaskManager sınıfı bitirildi.

--- TaskManager.java
+++ TaskManager.java
@@ -10,7 +10,7 @@ public TaskManager(){
 }
  public void addTask(Task task){
     tasks.add(task); // listeye ekle
-    System.out.println("Task added successfulyy");
+    System.out.println("Task added successfully");
  }
 
  public Task getTask(int id){
@@ -22,6 +22,39 @@ public TaskManager(){
     }
     return null; //bulunamadı
  }
+ // ıterator kullanarak silme
+public boolean removeTask(int id){
+    Iterator<Task> it = tasks.iterator();
+    while(it.hasNext()){
+        Task t =  it.next();
+        if(t.getId()==id){
+            it.remove();
+            System.out.println("Task removed.");
+            return true;
+        }
+    }
+    return false; // bulunamadı
+}
+// tüm listeyi yazdırma
+public void listTasks(){
+    if(tasks.isEmpty()){
+        System.out.println("no tasks available");
+        return;
+    }
+    for(Task t : tasks){
+        System.out.println(t); // toString methodu çağırılıyor
+        System.out.println(" -------------------");
+    }
+}
+
+// durum güncelleme
+public boolean toggleTaskStatus(int id){
+    Task t = getTask( id);
+    if(t==null) return false;
+    t.toggleCompleted();
+    System.out.println("Task status updated.");
+    return true;
+}
 
 
 }
